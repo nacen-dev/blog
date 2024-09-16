9692493401cb3abdd15dcc4cc0f8a9795582db6b
Vincent <nacen.dev@gmail.com>
2024-09-16 20:57:12 +0800

    Remove cn function from dateUtil

--- src/lib/dateUtils.ts
+++ src/lib/dateUtils.ts
@@ -1,10 +1,3 @@
-import { clsx, type ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
-
 export function formatDate(date: string, includeRelative = false) {
   let currentDate = new Date();
 
