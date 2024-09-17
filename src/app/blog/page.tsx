82ab65cf968c44c95a99ae73590e6ce5cc81ae66
Vincent <nacen.dev@gmail.com>
2024-09-17 18:29:12 +0800

    Update implementation

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,18 +1,5 @@
 import { BlogPosts } from "@/components/ui/blog-posts";
-import { DAY_IN_SECONDS } from "@/lib/dateUtils";
-import { getBlogPosts, MDXData } from "./util";
-
-// Revalidate cache after a day
-export const revalidate = DAY_IN_SECONDS;
-
-// We'll prerender only the params from `generateStaticParams` at build time.
-// If a request comes in for a path that hasn't been generated,
-// Next.js will server-render the page on-demand.
-export const dynamicParams = true; // or false, to 404 on unknown paths
-
-export async function generateStaticParams() {
-  return await getBlogPosts();
-}
+import { getBlogPosts } from "./util";
 
 export const metadata = {
   title: "Vincent's blog",
@@ -20,10 +7,7 @@ export const metadata = {
     "Explore a diverse range of topics on my personal blog, from programming and web development to books, games, and personal insights, as I share my journey of learning and discovery.",
 };
 
-type Props = {
-  blogPosts: MDXData[];
-};
-
-export default async function BlogPage({ blogPosts }: Props) {
+export default async function BlogPage() {
+  const blogPosts = await getBlogPosts();
   return <BlogPosts blogPosts={blogPosts} />;
 }
